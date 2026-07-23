a9d6f99d443b93e668633361229c66d4e78c3c9b
kamilnowak <kamil0nowak@yahoo.com>
2026-07-23 20:39:00 +0200

    Pundit barebones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include Pundit::Authorization
+
+  after_action :verify_authorized, except: [:index]
+  after_action :verify_policy_scoped, only: [:index]
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Nie masz uprawnień do wykonania tej akcji."
+    redirect_to(request.referer || root_path)
+  end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,11 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
-    @courses = Course.published.all
+    @courses = policy_scope(Course)
   end
 
   def show
     @course = Course.find(params[:id])
+    authorize @course
   end
 end
