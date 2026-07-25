3bda9ad5b967663ddc3c91d2f5bb88117002f769
kamilnowak <kamil0nowak@yahoo.com>
2026-07-25 16:52:55 +0200

    Custom confirm modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+Turbo.setConfirmMethod((message, element) => {
+    const dialog = document.getElementById("turbo-confirm")
+    const messageEl = document.getElementById("turbo-confirm-message")
+
+    if (messageEl) {
+        messageEl.textContent = message
+    }
+
+    dialog.showModal()
+
+    return new Promise((resolve) => {
+        dialog.addEventListener("close", () => resolve(dialog.returnValue === "confirm"))
+    }, { once: true } )
+})
