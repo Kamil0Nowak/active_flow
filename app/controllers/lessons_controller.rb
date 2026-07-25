e0d526a6645a72bd4824fc05a4e511e9ed17cf82
kamilnowak <kamil0nowak@yahoo.com>
2026-07-25 17:15:39 +0200

    Fix for a bug in the lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,14 @@
 class LessonsController < ApplicationController
-  before_action :set_course_and_lesson, only: [ :create, :edit, :update, :destroy]
+  before_action :set_course, only: [:new, :create]
+  before_action :set_course_and_lesson, only: [:edit, :update, :destroy]
+
   def new
-    @course = Course.find(params[:course_id])
     @lesson = @course.lessons.build
     authorize @lesson
   end
 
   def create
+    @lesson = @course.lessons.build(lesson_params)
     authorize @lesson
     if @lesson.save
       redirect_to course_path(@course), notice: "Lesson was successfully created."
@@ -21,7 +23,6 @@ class LessonsController < ApplicationController
 
   def update
     authorize @lesson
-
     if @lesson.update(lesson_params)
       redirect_to course_path(@course), notice: "Lesson was successfully updated."
     else
@@ -36,8 +37,13 @@ class LessonsController < ApplicationController
   end
 
   private
+
   def lesson_params
     params.require(:lesson).permit(:title, :description, :start_time, :end_time)
+  end
+
+  def set_course
+    @course = Course.find(params[:course_id])
   end
 
   def set_course_and_lesson
