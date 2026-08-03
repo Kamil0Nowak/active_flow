b50fef4cccb959405e5f4917ca89ff1ee95bfacc
kamilnowak <kamil0nowak@yahoo.com>
2026-08-03 20:40:05 +0200

    Added missing validations in lesson & some ui tweaks

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,7 +4,13 @@ class Lesson < ApplicationRecord
   has_many :users, through: :enrollments
 
   validates :title, :start_time, :end_time, presence: true
-  validates :end_time, comparison: { greater_than: :start_time }
+  validate :end_date_after_start_date
 
   scope :upcoming, -> { where("start_time >= ?", Time.current).order(start_time: :asc) }
+
+  private
+  def end_date_after_start_date
+    return if start_time.blank? || end_time.blank?
+    errors.add(:end_time, "must occur after start time") if end_time < start_time
+  end
 end
