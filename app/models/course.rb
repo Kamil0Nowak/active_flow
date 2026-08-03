d7a126f53192e139c6b4ce0056bd527a7dd49435
kamilnowak <kamil0nowak@yahoo.com>
2026-08-03 17:51:56 +0200

    Capacity now accually works

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,14 +2,14 @@ class Course < ApplicationRecord
   belongs_to :teacher, class_name: "User"
   has_many :lessons, dependent: :destroy
   has_many :enrollments, through: :lessons
-  enum :status, { draft: 0, published: 1 }
+  enum :status, { draft: 0, published: 1 } # not used yet
 
   validates :title, :start_date, :end_date, :capacity, presence: true
   validates :capacity, numericality: { greater_than: 0 }
   validate :end_date_after_start_date
 
   def self.ransackable_attributes(auth_object = nil)
-    ["title"]
+    [ "title" ]
   end
 
   def visible_lessons_for(user)
@@ -24,8 +24,12 @@ class Course < ApplicationRecord
     !full?
   end
 
+  def actual_capacity
+    capacity - enrollments.where.not(status: :rejected).count
+  end
+
   private
   def end_date_after_start_date
-    errors.add(:end_date, "must be after start date") if end_date < start_date
+    errors.add(:end_date, "musi nastąpić po dacie startu") if end_date < start_date
   end
 end
